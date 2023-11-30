ad8420b09bf502a25d46161ed9850125337a010b
omer mahdi <iam.omer.mahdi@gmail.com>
2023-11-30 09:23:51 +0200

    lazy loading images

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -19,10 +19,12 @@
             <Skeleton v-if="loading" />
 
             <div class="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-6" v-else>
-                <article class="card bg-base-100 border border-base-200 rounded-sm relative" v-for="country in filteredCountries.slice(0, slice)" :key="country.name">
+                <article class="card bg-base-100 border border-base-200 rounded-sm relative" v-for="(country, index) in filteredCountries.slice(0, slice)" :key="country.name">
                     
                     <RouterLink tag="a" :to="`/country/${country.alpha3Code}`">
-                    <figure class="border border-base-200"><img fetchpriority="high" :src="country.flag" class="w-full object-cover h-56" :alt="country.name + ' flag'" /></figure>
+                    <figure class="border border-base-200">
+                        <img loading="lazy" :src="country.flag" class="w-full object-cover h-56" :alt="country.name + ' flag'" />
+                    </figure>
                     <div class="card-body p-0 md:p-4">
                         <table class="table">
                             <thead>
